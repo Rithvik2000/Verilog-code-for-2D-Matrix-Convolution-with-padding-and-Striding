2cac9511e804f31bf17f83cf590e210838c596ec
AILA TEJ RITHVIK <ailatejrithvik2000@gmail.com>
2025-07-20 16:21:26 +0530

    Update 2D_conv.v

--- 2D_conv.v
+++ 2D_conv.v
@@ -1,12 +1,9 @@
-`timescale 1ns / 1ps
-////////////////////////////////////////////////////////////////////////////////
 
-
-module twoD_conv #(parameter Width = 16 )(
+module Two_D_Conv_with_S_and_P #(parameter Width = 16 )(
  input[Width-1:0] A_in,
  input[Width-1:0] B_in,
  input RST,CLK,
- input[5:0] N1, M1, S1,
+ input[5:0] N1, M1, S1, P1, O1,
  
   
    output reg [Width-1:0] FINAL_OUT,
@@ -16,17 +13,35 @@ module twoD_conv #(parameter Width = 16 )(
  reg signed [Width-1:0] A[63:0];
  reg signed [Width-1:0] B[63:0];
  reg signed [Width-1:0] OUT[63:0];
+ 
  reg [3:0] State , nxt_State ;
  reg [Width-1:0] ACC;
  reg [5:0] Out_size ;
  
  reg [5:0] Count_1, Count_2, Count_3,Count_4,Count_5; 
- reg [5:0] N, M ,S;
+ reg [5:0] N, M ,S,P,N_padded;
  //wire [3:0] w1;
+ 
+ localparam s0 = 4'b0000,
+            s1 = 4'b0001,
+            s2 = 4'b0010,
+            s3 = 4'b0011,
+            s4 = 4'b0100,
+            s5 = 4'b0101;
+ 
+ integer i;
  
  always@(posedge CLK or posedge RST) begin
  if (RST == 1)
- State <= 4'b0000;
+ begin
+ State <= s0;
+  for (i = 0; i < 64 ; i = i+1)
+   begin
+    A[i] <= 0;
+    B[i] <= 0;
+    OUT[i] <= 0;
+   end
+ end
  else
  begin
  
@@ -37,7 +52,7 @@ module twoD_conv #(parameter Width = 16 )(
  case (State)
   
                  
-  4'b0000: begin             //// Reset
+  s0    : begin             //// Reset
             Count_1  <= 0;
             Count_2 <= 0;
             Count_3 <=0;
@@ -46,29 +61,53 @@ module twoD_conv #(parameter Width = 16 )(
             Done <= 0;
             FINAL_OUT <= 0;
             ACC <= 0;
+            Out_size <= (O1)   *  (O1) ;
+             N <= N1;
+              M <= M1;
+              S <= S1;
+              P <= P1;
+              N_padded <= 2*P1 + N1; 
           end
           
-   4'b0001: begin                //// writing in Data
+   s1    : begin       
+              Count_1 <= Count_1 + 1'b1;
+   
+            //// writing in Data
               if(Count_1 < N*N )
-              A[Count_1] <= A_in;
+              A[Count_1 + P*(N + 2*P + 1) + Count_3] <= A_in;
               
               if(Count_1 < M*M )
-              begin
               B[Count_1] <= B_in;
-               end
-               
+                             
              // A[Count_1] <= 0;
              //A[Count_1 + N+M] <= 0;
              
                
-               Count_1 <= Count_1 + 1'b1;
+               
               if(Count_1 == N*N)
+               begin
                 Count_1 <= 0;
+                Count_2 <= 0;
+                Count_3 <= 0;
+               end
+               
+             else if(Count_2 == N-1 )
+               begin
+               Count_3 <= Count_3 + 2*P;
+               Count_2 <= 0;
+               end
+             else
+                Count_2 <= Count_2 + 1'b1;
+                
+                
+                
+               
+                
           end
        
-        
-   4'b0010 : begin  
-             ACC <= (ACC + A[Count_1 + N*Count_2 + Count_3 + N*Count_4 ]*B[Count_1 + M*Count_2 ]);
+      
+   s2   : begin  
+             ACC <= (ACC + A[Count_1 + N_padded *Count_2 + Count_3 + N_padded *Count_4 ]*B[Count_1 + M*Count_2 ]);
               
                    if (Count_1 == M-1)
                          begin 
@@ -83,15 +122,15 @@ module twoD_conv #(parameter Width = 16 )(
                 
                 
              end 
-   4'b0011 :   OUT[ Count_5 ]  <= ACC;
+   s3 :   OUT[ Count_5 ]  <= ACC;
                   
-   4'b0100 :  begin
+   s4 :  begin
                 ACC <= 0;
                 Count_1 <= 0; 
                 Count_2 <= 0;
                 Count_5 <= Count_5 + 1;
                 Count_3 <= Count_3 + S;
-                             if ( Count_3 == N-M)
+                             if ( Count_3 == N_padded -M)
                              begin
                              Count_4 <= Count_4 + S;
                              Count_3 <= 0;
@@ -103,7 +142,7 @@ module twoD_conv #(parameter Width = 16 )(
            end
            
          
-   4'b0101 :    begin
+   s5 :    begin
                 if (Count_5 > 0) 
                  begin
                 FINAL_OUT <= OUT[Out_size- (Count_5 )];
@@ -127,11 +166,9 @@ module twoD_conv #(parameter Width = 16 )(
  
  always@(*) begin
     
-  N = N1;
-  M = M1;
-  S = S1;
+ 
   
-  Out_size = ((N-M)/S + 1)*((N-M)/S + 1);
+
   
  case(State)
 
@@ -163,7 +200,7 @@ module twoD_conv #(parameter Width = 16 )(
           end        
   4'b0011 : nxt_State =   4'b0100;        
   4'b0100: begin
-           if (Count_4 == N-M && Count_3 == N-M)
+           if (Count_4 == N_padded-M && Count_3 == N_padded-M)
            nxt_State = 4'b0101; 
           
            
@@ -180,3 +217,4 @@ module twoD_conv #(parameter Width = 16 )(
  end
 
 endmodule
+
